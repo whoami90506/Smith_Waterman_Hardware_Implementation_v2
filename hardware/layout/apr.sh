e8201f962f8e72d3b0acdb5fe55ddbe1fdfcab6d
whoami90506 <whoami90506@github.com>
2019-12-16 14:38:21 +0800

    add pi control add temp

--- hardware/layout/apr.sh
+++ hardware/layout/apr.sh
@@ -1,12 +1,12 @@
-echo ============================================ | tee autosave/apr.log
-date '+%m/%d %A %H:%M:%S' | tee -a autosave/apr.log
-echo start innovus | tee -a autosave/apr.log
-echo ============================================ | tee -a autosave/apr.log
-innovus -files apr.tcl -no_gui | tee -a autosave/apr.log
+echo ============================================ | tee apr.log
+date '+%m/%d %A %H:%M:%S' | tee -a apr.log
+echo start innovus | tee -a apr.log
+echo ============================================ | tee -a apr.log
+innovus -files apr.tcl -no_gui | tee -a apr.log
 
-echo ============================================ | tee -a autosave/apr.log
-date '+%m/%d %A %H:%M:%S' | tee -a autosave/apr.log
-echo finish innovus | tee -a autosave/apr.log
-echo ============================================ | tee -a autosave/apr.log
+echo ============================================ | tee -a apr.log
+date '+%m/%d %A %H:%M:%S' | tee -a apr.log
+echo finish innovus | tee -a apr.log
+echo ============================================ | tee -a apr.log
 
 exit
