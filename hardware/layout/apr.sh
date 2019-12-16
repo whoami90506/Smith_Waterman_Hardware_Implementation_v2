f10056758d2dcbaef52da9746ba83fe268956591
whoami90506 <whoami90506@github.com>
2019-12-16 14:02:36 +0800

    correct name rm autoreport fanout

--- hardware/layout/apr.sh
+++ hardware/layout/apr.sh
@@ -1,12 +1,12 @@
-echo ============================================ | tee autoreport/apr.log
-date '+%m/%d %A %H:%M:%S' | tee -a autoreport/apr.log
-echo start innovus | tee -a autoreport/apr.log
-echo ============================================ | tee -a autoreport/apr.log
-innovus -files apr.tcl -no_gui | tee -a autoreport/apr.log
+echo ============================================ | tee autosave/apr.log
+date '+%m/%d %A %H:%M:%S' | tee -a autosave/apr.log
+echo start innovus | tee -a autosave/apr.log
+echo ============================================ | tee -a autosave/apr.log
+innovus -files apr.tcl -no_gui | tee -a autosave/apr.log
 
-echo ============================================ | tee -a autoreport/apr.log
-date '+%m/%d %A %H:%M:%S' | tee -a autoreport/apr.log
-echo finish innovus | tee -a autoreport/apr.log
-echo ============================================ | tee -a autoreport/apr.log
+echo ============================================ | tee -a autosave/apr.log
+date '+%m/%d %A %H:%M:%S' | tee -a autosave/apr.log
+echo finish innovus | tee -a autosave/apr.log
+echo ============================================ | tee -a autosave/apr.log
 
 exit
